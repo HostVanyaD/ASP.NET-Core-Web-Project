cdba724a7012bf4601c33085a398aec048497f02
HostVanyaD <reeedruuum@gmail.com>
2022-02-25 15:38:21 +0200

    Added home/index view

--- HighPaw.Web/HighPaw.Web/Controllers/HomeController.cs
+++ HighPaw.Web/HighPaw.Web/Controllers/HomeController.cs
@@ -1,14 +1,36 @@
 ﻿namespace HighPaw.Web.Controllers
 {
+    using HighPaw.Data;
     using HighPaw.Web.Models;
+    using HighPaw.Web.Models.Pets;
     using Microsoft.AspNetCore.Mvc;
     using System.Diagnostics;
+    using System.Linq;
 
     public class HomeController : Controller
     {
+        private readonly HighPawDbContext data;
+
+        public HomeController(HighPawDbContext data)
+            => this.data = data;
+
         public IActionResult Index()
         {
-            return View();
+            var allPets = this.data
+                .Pets
+                .Select(p => new PetListingViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Age = p.Age,
+                    ImageUrl = p.ImageUrl,
+                    Shelter = p.Shelter.Address
+                })
+                .OrderBy(p => p.Id)
+                .Take(4)
+                .ToList();
+
+            return View(allPets);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ HighPaw.Web/HighPaw.Web/Models/Pets/PetListingViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HighPaw.Web.Models.Pets
+{
+    public class PetListingViewModel
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+
+        public string ImageUrl { get; set; }
+
+        public int? Age { get; set; }
+
+        public string Sex { get; set; }
+
+        public string Shelter { get; set; }
+    }
+}
